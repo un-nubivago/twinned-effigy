e60fc36d373a8eda1a36ded447b99a03c12d7c23
Nubìvago <fradige95@gmail.com>
2026-05-09 14:03:23 +0200

    Create project

---
+++ src/client/java/niv/twinnedeffigy/client/TwinnedBEffigyDataGenerator.java
@@ -0,0 +1,15 @@
+package niv.twinnedeffigy.client;
+
+import org.jspecify.annotations.NullMarked;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+@NullMarked
+public class TwinnedBEffigyDataGenerator implements DataGeneratorEntrypoint {
+
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        // TODO
+    }
+}
---
+++ src/main/java/niv/twinnedeffigy/TwinnedEffigy.java
@@ -0,0 +1,27 @@
+package niv.twinnedeffigy;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.jspecify.annotations.NullMarked;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@NullMarked
+public class TwinnedEffigy implements ModInitializer {
+
+    public static final String MOD_ID = "twinned-effigy";
+
+    public static final String MOD_NAME = "Twinned Effigy";
+
+    @SuppressWarnings("null")
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
+
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+
+        LOGGER.info("[{}] Initialize", MOD_NAME);
+    }
+}
