da9aec0590fb53177ad6e20658dae56d82eb3a7d
Nubìvago <fradige95@gmail.com>
2026-09-09 17:51:53 +0200

    Add sed rule to turn marked tasks to strike-through items

--- README.sh
+++ README.sh
@@ -5,4 +5,5 @@ cat README.md \
         \_\[!\[The Crisis in Storage Tech\](https://img.youtube.com/vi/GyUL3PbjWcw/maxresdefault.jpg)\](https://www.youtube.com/watch?v=GyUL3PbjWcw)_d;
         s_<!--\(<iframe.*iframe>\)-->_\1_;
         s_img/Twinned\_Effigy\_Recipe.png_https://cdn.modrinth.com/data/RWCrb0FL/images/9bd77766e59772770fada3fb5658727acac2cc4e.png_;
-        s_- \[.\] _- _g;'
+        s_- \[ \] _- _g;
+        s_- \[x\] \(.*\)$_- ~~\1~~_g;'
